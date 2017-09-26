861ade9afdbdbb6b2961d0f8cf40b36c0ef2edc5
danielstaal <daniel.staal@hotmail.com>
2017-09-26 16:13:38 +0200

    hyperparams

--- Population.java
+++ Population.java
@@ -5,32 +5,65 @@ import org.vu.contest.ContestEvaluation;
 
 public class Population
 {
-	private int popCount;
 	private ArrayList<RealGenotype> population = new ArrayList<RealGenotype>(); 
-	private int noOfSurvivors = 20;
+	private int noOfSurvivors = 0;
+	private String evaluationType = "";
+	private double evaluationLimit = 0.0;
 	ContestEvaluation evaluation_;
 
+	//////// hyperparameters to be set according to the type of function
+    int populationSize = 0;
+    int noOfGenerations = 0;
+    // standard deviation for the gaussian selecting the parents
+	double selection_std = 0;
 
-	public Population(int popcount, ContestEvaluation evaluation){
-		popCount = popcount;
-		initPopulation();
+
+	public Population(String evType, ContestEvaluation evaluation, int evLimit){
 		evaluation_ = evaluation;
+		evaluationType = evType;
+		evaluationLimit = evLimit;
+		setHyperparameters();
+
+		initPopulation();
 	}
+
+
+	private void setHyperparameters(){
+		System.out.println(evaluationType);
+		if(evaluationType.equals("Multimodal")){
+			populationSize = 100;
+			selection_std = 30;
+			noOfGenerations = (int)(evaluationLimit/populationSize);
+			noOfSurvivors = 20;
+		}else if(evaluationType.equals("Regular")){
+            populationSize = 1;
+			selection_std = 1;
+			noOfGenerations = (int)(evaluationLimit/populationSize);
+			noOfSurvivors = 1;
+        }else{
+            populationSize = 50;
+			selection_std = 10;
+			noOfGenerations = (int)(evaluationLimit/populationSize);
+			noOfSurvivors = 10;
+        }
+	}
+
+
 	private void initPopulation(){
-		for(int i=0;i<popCount;i++){
+		for(int i=0;i<populationSize;i++){
         	population.add(new RealGenotype(-5,5));
         }
 	}
 
 	public void nextGeneration(){
 		selection();
-		System.out.println(population.get(99).getFitness());
+		// System.out.println(population.get(populationSize-1).getFitness());
 		// System.out.println(Arrays.toString(sel));
 		recombinationAndMutation();
 	}
 
 	private void selection(){
-		for(int i=0;i<popCount-noOfSurvivors;i++){
+		for(int i=0;i<populationSize-noOfSurvivors;i++){
 			population.get(i).setFitness((double) evaluation_.evaluate(population.get(i).getValue()));
 		}
 		// sort the population according to fitness
@@ -43,12 +76,12 @@ public class Population
 		ArrayList<RealGenotype> new_population = new ArrayList<>();
 		int mom_idx = 0;
 		int dad_idx = 0;
-		for(int i=0;i<popCount - noOfSurvivors;i++){
+		for(int i=0;i<populationSize - noOfSurvivors;i++){
 			do{
-				mom_idx = 99 - (int) Math.abs(r.nextGaussian()*30);
+				mom_idx = (populationSize-1) - (int) Math.abs(r.nextGaussian()*selection_std);
 			} while (mom_idx < 0);
 			do{
-				dad_idx = 99 - (int) Math.abs(r.nextGaussian()*30);
+				dad_idx = (populationSize-1) - (int) Math.abs(r.nextGaussian()*selection_std);
 			} while (mom_idx == dad_idx || dad_idx < 0);
 
 			RealGenotype child = RealGenotype.breed2(population.get(mom_idx), population.get(dad_idx));
@@ -59,7 +92,7 @@ public class Population
 			new_population.add(child);
 		}
 		// add the parents to the next generation
-		for(int i=popCount-noOfSurvivors;i<popCount;i++){
+		for(int i=populationSize-noOfSurvivors;i<populationSize;i++){
 			new_population.add(population.get(i));
 		}
 		population = new_population;
@@ -67,5 +100,9 @@ public class Population
 
     public ArrayList<RealGenotype> getPopulation(){
     	return population;
+    }
+
+    public int getNoOfGenerations(){
+    	return noOfGenerations;
     }
 }
