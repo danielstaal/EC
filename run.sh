5e0b949d1167a1134df29e65d36ededb8fff261f
danielstaal <daniel.staal@hotmail.com>
2017-10-15 16:21:00 +0200

    pull

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.libcjavabbob.so
 
 javac -cp contest.jar group39.java RealGenotype.java Population.java
 jar cmf MainClass.txt submission.jar group39.class RealGenotype.class Population.class
 
 
-java -jar testrun.jar -submission=group39 -evaluation=SchaffersEvaluation -seed=1 3
+java -Dtest=1 -Dtest2=2 -jar testrun.jar -submission=group39 -evaluation=BentCigarFunction -seed=1
